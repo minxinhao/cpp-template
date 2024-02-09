ca4490b06f916b7046abb08fb36deb4f7f4a33ad
XinHao Min <mxh@s22.pdsl.local>
2024-02-09 13:34:34 +0800

    add test recursive

--- test_recursive.cc
+++ test_recursive.cc
@@ -1,16 +1,21 @@
 #include "dbg.h"
 
-template <typename T, int stack_size = 0>
+template <typename T, int stack_size>
 T func(T &&t)
-    requires(stack_size >= 0)
+    requires(stack_size > 0)
 {
     if (stack_size == 0)
         return t;
     return t * func<T, stack_size - 1>(t - 1);
 }
 
+template <typename T, int = 0> T func(T &&t)
+{
+    return t;
+}
+
 int main()
 {
-    func(1);
+    dbg(func(1));
     return 0;
 }
