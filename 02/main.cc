f1cc214cad70fd5dd2ea562c9d0ac3358739f79a
XinHao Min <mxh@s22.pdsl.local>
2024-02-10 10:44:20 +0800

    add 02 for std format and User-defined literals

--- 02/main.cc
+++ 02/main.cc
@@ -1,10 +1,15 @@
 #include "dbg.h"
 #include <stddef.h>
-#include <format>
+// #include <format> // format is not supported by current compiler
 
 void operator""_get_string(const char *str, size_t len)
 {
     dbg(str,len);
+    // std::format
+}
+
+constexpr auto operator""_f(const char* fmt, size_t) {
+    return[=]<typename... T>(T&&... Args) { return std::vformat(fmt, std::make_format_args(std::forward<T>(Args)...)); };
 }
 
 int main()
