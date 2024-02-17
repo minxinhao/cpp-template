8e231649cb7575a05c37fe2aebd2207172e81b53
minxinhao <550279741@mail.com>
2024-02-17 14:35:02 +0800

    learn used init and template to detect number of members in an abrevate

---
+++ test_template/test_tuple.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <tuple>
+
+struct MyStruct {
+    int a;
+    double b;
+    char c;
+};
+
+int main() {
+    std::cout << "Member count: " << std::tuple_size<MyStruct>::value << std::endl;
+    
+    return 0;
+}
